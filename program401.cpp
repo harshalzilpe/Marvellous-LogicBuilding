23e78ccbd12004186a09dc3ffe4ea9fd0771d687
Harshal Zilpe <108272214+harshalzilpe@users.noreply.github.com>
2025-05-13 21:02:41 +0530

    Add files via upload

---
+++ program401.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+template<class T>
+T AdditionD(T Arr[], int iSize)
+{
+    T dSum;
+    int i = 0;
+    
+    for(i = 0; i < iSize ; i++)
+    {
+        dSum = dSum + Arr[i];
+    }
+    return dSum;
+}
+
+int main()
+{
+    double Brr[] = {10.01,20.20,30.03,40.40};
+    double dRet = 0;
+
+    dRet = AdditionD(Brr,4);
+
+    cout<<"Addition of double values are "<<dRet<<".\n";
+
+    return 0;
+}
